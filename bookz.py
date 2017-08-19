abeb55f7a8861f2dcc34ef9222024a9a7fdb794a
Evan Benn <evanbenn@gmail.com>
2017-08-19 12:01:01 -0700

    mostly working interactive interface

--- bookz.py
+++ bookz.py
@@ -17,35 +17,56 @@ import struct
 import sys
 import argparse
 import shlex
+import select
 
 import jaraco.logging
 
 import irc.client
 
 class DCCReceive(irc.client.SimpleIRCClient):
-    def __init__(self, search):
+    def __init__(self, channel):
         irc.client.SimpleIRCClient.__init__(self)
         self.received_bytes = 0
         self.users = set()
-        self.search = search
-        self.search_done = False
+        self.channel = channel
+        self.expect_book = False
+
+    def msg(self, msg):
+        # connect -> search -> dcc recv -> choice list -> dcc recv  
+        #                ^\ -----------  </ ----------   </  
+        if self.expect_book:
+            try:
+                choice = int(msg)
+            except ValueError as e:
+                print("searching", msg)
+                self.expect_book = False
+                self.connection.privmsg(self.channel, "@search " + msg)
+            else:
+                print("sending", self.getters[choice])
+                self.connection.privmsg(self.channel, self.getters[choice])
+        else:
+            print("searching", msg)
+            self.connection.privmsg(self.channel, "@search " + msg)
 
     def on_ctcp(self, connection, event):
         payload = event.arguments[1]
         parts = shlex.split(payload)
-        command, filename, peer_address, peer_port, size = parts
-        if command != "SEND":
-            return
-        self.filename = os.path.basename(filename)
-        if os.path.exists(self.filename):
-            print("A file named", self.filename,
-                "already exists. Refusing to save it.")
-            self.connection.quit()
-            return
-        self.file = open(self.filename, "wb")
-        peer_address = irc.client.ip_numstr_to_quad(peer_address)
-        peer_port = int(peer_port)
-        self.dcc = self.dcc_connect(peer_address, peer_port, "raw")
+        try:
+            command, filename, peer_address, peer_port, size = parts
+        except ValueError as e:
+            print("failed to unpack:", parts)
+        else:
+            if command != "SEND":
+                return
+            self.filename = os.path.basename(filename)
+            if os.path.exists(self.filename):
+                print("A file named", self.filename, "already exists. Refusing to save it.")
+                return
+            print("downloading", self.filename, size)
+            self.file = open(self.filename, "wb")
+            peer_address = irc.client.ip_numstr_to_quad(peer_address)
+            peer_port = int(peer_port)
+            self.dcc = self.dcc_connect(peer_address, peer_port, "raw")
 
     def on_dccmsg(self, connection, event):
         data = event.arguments[0]
@@ -55,10 +76,8 @@ class DCCReceive(irc.client.SimpleIRCClient):
 
     def on_dcc_disconnect(self, connection, event):
         self.file.close()
-        if not self.search_done:
-            self.search_done = True
-            print("Received file %s (%d bytes)." % (self.filename,
-                                                    self.received_bytes))
+        print("Received file %s (%d bytes)." % (self.filename, self.received_bytes))
+        if not self.expect_book:
             try:
                 zipped = zipfile.ZipFile(self.filename)
             except zipfile.BadZipFile as e:
@@ -68,25 +87,27 @@ class DCCReceive(irc.client.SimpleIRCClient):
             for f in zipped.namelist():
                 getters += [x for x in zipped.open(f).read().splitlines() if x.startswith('!')]
 
-            for name in getters:
-                n = name.split()[0][1:]
-                if n in self.users:
-                    print ("sending", name)
-                    self.connection.privmsg("#bookz", name)
-                    return
-            else:
-                print ("no good things")
+            self.getters = [g for g in getters if g.split()[0][1:] in self.users]
+            for i,g in enumerate(self.getters):
+                print(i, g)
+            print ("CHOOSE")
+            self.expect_book = True
+
+            zipped.close()
         else:
             # got a book i guess
             if self.filename.endswith('rar'):
                 rarfile.RarFile(self.filename).extractall()
+                os.remove(self.filename)
             if self.filename.endswith('zip'):
                 zipfile.ZipFile(self.filename).extractall()
-
-
-        # self.connection.quit()
+                os.remove(self.filename)
+            
+            self.expect_book = False
 
     def on_disconnect(self, connection, event):
+        print("disconnect")
+        print(event)
         sys.exit(0)
 
     def on_namreply(self, c, e):
@@ -116,21 +137,17 @@ class DCCReceive(irc.client.SimpleIRCClient):
 
     def on_welcome(self, connection, event):
         print ("Connected")
-        connection.join("#bookz")
+        connection.join(self.channel)
 
     def on_join(self, connection, event):
-        connection.privmsg("#bookz", "@search " + self.search)
-        connection.names(["#bookz"])
+        connection.names([self.channel])
 
 
 def get_args():
-    parser = argparse.ArgumentParser(
-        description="Receive a single file to the current directory via DCC "
-            "and then exit.",
-    )
+    parser = argparse.ArgumentParser()
     parser.add_argument('server')
+    parser.add_argument('channel')
     parser.add_argument('nickname')
-    parser.add_argument('search')
     parser.add_argument('-p', '--port', default=6667, type=int)
     jaraco.logging.add_arguments(parser)
     return parser.parse_args()
@@ -139,17 +156,25 @@ def main():
     args = get_args()
     jaraco.logging.setup(args)
 
-    c = DCCReceive(args.search)
+    c = DCCReceive(args.channel)
     try:
         c.connect(args.server, args.port, args.nickname)
     except irc.client.ServerConnectionError as x:
         print(x)
         sys.exit(1)
 
-    # con.add_global_handler("welcome", on_connect)
-    # con.add_global_handler("join", on_join)
+    reactor = c.reactor
+    
+    while True:
+        reactor.process_once(0.2)
+        while sys.stdin in select.select([sys.stdin], [], [], 0)[0]:
+            line = sys.stdin.readline()
+            if line:
+                c.msg(line.strip())
+            else: # an empty line means stdin has been closed
+                print('eof')
+                exit(0)
 
-    c.start()
 
 if __name__ == "__main__":
     main()
