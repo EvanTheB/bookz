378aef8db081da9a163bae45d8365178fec61388
EvanTheB <evanbenn@gmail.com>
2017-09-11 22:56:06 +0800

    solved weird libirc bug - update. Think the code is still incorrect. (strncmp without strlen being used to check prefixes...) Pare down code, working join and DCC downloader.

--- bookz.c
+++ bookz.c
@@ -31,6 +31,7 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <errno.h>
+#include <unistd.h>
 
 #include "libircclient.h"
 
@@ -46,54 +47,12 @@ typedef struct
 } irc_ctx_t;
 
 
-void addlog (const char * fmt, ...)
-{
-    FILE * fp;
-    char buf[1024];
-    va_list va_alist;
-
-    va_start (va_alist, fmt);
-#if defined (WIN32)
-    _vsnprintf (buf, sizeof(buf), fmt, va_alist);
-#else
-    vsnprintf (buf, sizeof(buf), fmt, va_alist);
-#endif
-    va_end (va_alist);
-
-    printf ("%s\n", buf);
-
-    if ( (fp = fopen ("irctest.log", "ab")) != 0 )
-    {
-        fprintf (fp, "%s\n", buf);
-        fclose (fp);
-    }
-}
-
-
-void dump_event (irc_session_t * session, const char * event, const char * origin, const char ** params, unsigned int count)
-{
-    char buf[512];
-    int cnt;
-
-    buf[0] = '\0';
-
-    for ( cnt = 0; cnt < count; cnt++ )
-    {
-        if ( cnt )
-            strcat (buf, "|");
-
-        strcat (buf, params[cnt]);
-    }
-
-
-    addlog ("Event \"%s\", origin: \"%s\", params: %d [%s]", event, origin ? origin : "NULL", cnt, buf);
-}
-
 struct thread_args
 {
     irc_session_t *session;
+    char *channel;
 };
-static void * thread_function (void * vargs)
+static void * thread_function(void * vargs)
 {
     struct thread_args *args = vargs;
     while ( 1 )
@@ -101,21 +60,25 @@ static void * thread_function (void * vargs)
         char buffer[200];
         char *buf = buffer;
         size_t buffer_size = 200;
-        getline(&buf,&buffer_size,stdin);
-        irc_cmd_msg (args->session, "#channel", buf);
+        getline(&buf, &buffer_size,stdin);
+        irc_cmd_msg (args->session, args->channel, buf);
     }
+    free(args->channel);
+    free(vargs);
     return 0;
 }
 
 void event_join (irc_session_t * session, const char * event, const char * origin, const char ** params, unsigned int count)
 {
-    dump_event (session, event, origin, params, count);
+    printf("joined channel\n");
     irc_cmd_user_mode (session, "+i");
-    irc_cmd_msg (session, params[0], "Hi all");
 
     pthread_t tid;
     struct thread_args* ta = malloc(sizeof(ta));
     ta->session = session;
+    ta->channel = malloc(strlen(params[0]) + 1);
+    strcpy(ta->channel, params[0]);
+
     if (pthread_create (&tid, 0, thread_function, ta) != 0)
         printf ("CREATE_THREAD failed: %s\n", strerror(errno));
 }
@@ -123,8 +86,9 @@ void event_join (irc_session_t * session, const char * event, const char * origi
 
 void event_connect (irc_session_t * session, const char * event, const char * origin, const char ** params, unsigned int count)
 {
+    printf("connected\n");
     irc_ctx_t * ctx = (irc_ctx_t *) irc_get_ctx (session);
-    dump_event (session, event, origin, params, count);
+    // dump_event (session, event, origin, params, count);
 
     irc_cmd_join (session, ctx->channel, 0);
 }
@@ -132,45 +96,10 @@ void event_connect (irc_session_t * session, const char * event, const char * or
 
 void event_privmsg (irc_session_t * session, const char * event, const char * origin, const char ** params, unsigned int count)
 {
-    dump_event (session, event, origin, params, count);
-
     printf ("'%s' said me (%s): %s\n",
         origin ? origin : "someone",
         params[0], params[1] );
 }
-
-
-void dcc_recv_callback (irc_session_t * session, irc_dcc_t id, int status, void * ctx, const char * data, unsigned int length)
-{
-    static int count = 1;
-    char buf[12];
-
-    switch (status)
-    {
-    case LIBIRC_ERR_CLOSED:
-        printf ("DCC %d: chat closed\n", id);
-        break;
-
-    case 0:
-        if ( !data )
-        {
-            printf ("DCC %d: chat connected\n", id);
-            irc_dcc_msg (session, id, "Hehe");
-        }
-        else
-        {
-            printf ("DCC %d: %s\n", id, data);
-            sprintf (buf, "DCC [%d]: %d", id, count++);
-            irc_dcc_msg (session, id, buf);
-        }
-        break;
-
-    default:
-        printf ("DCC %d: error %s\n", id, irc_strerror(status));
-        break;
-    }
-}
-
 
 void dcc_file_recv_callback (irc_session_t * session, irc_dcc_t id, int status, void * ctx, const char * data, unsigned int length)
 {
@@ -196,77 +125,6 @@ void dcc_file_recv_callback (irc_session_t * session, irc_dcc_t id, int status, 
     }
 }
 
-
-void event_channel (irc_session_t * session, const char * event, const char * origin, const char ** params, unsigned int count)
-{
-    char nickbuf[128];
-
-    if ( count != 2 )
-        return;
-
-    printf ("'%s' said in channel %s: %s\n",
-        origin ? origin : "someone",
-        params[0], params[1] );
-
-    if ( !origin )
-        return;
-
-    irc_target_get_nick (origin, nickbuf, sizeof(nickbuf));
-
-    if ( !strcmp (params[1], "quit") )
-        irc_cmd_quit (session, "of course, Master!");
-
-    if ( !strcmp (params[1], "help") )
-    {
-        irc_cmd_msg (session, params[0], "quit, help, dcc chat, dcc send, ctcp");
-    }
-
-    if ( !strcmp (params[1], "ctcp") )
-    {
-        irc_cmd_ctcp_request (session, nickbuf, "PING 223");
-        irc_cmd_ctcp_request (session, nickbuf, "FINGER");
-        irc_cmd_ctcp_request (session, nickbuf, "VERSION");
-        irc_cmd_ctcp_request (session, nickbuf, "TIME");
-    }
-
-    if ( !strcmp (params[1], "dcc chat") )
-    {
-        irc_dcc_t dccid;
-        irc_dcc_chat (session, 0, nickbuf, dcc_recv_callback, &dccid);
-        printf ("DCC chat ID: %d\n", dccid);
-    }
-
-    if ( !strcmp (params[1], "dcc send") )
-    {
-        irc_dcc_t dccid;
-        irc_dcc_sendfile (session, 0, nickbuf, "irctest.c", dcc_file_recv_callback, &dccid);
-        printf ("DCC send ID: %d\n", dccid);
-    }
-
-    if ( !strcmp (params[1], "topic") )
-        irc_cmd_topic (session, params[0], 0);
-    else if ( strstr (params[1], "topic ") == params[1] )
-        irc_cmd_topic (session, params[0], params[1] + 6);
-
-    if ( strstr (params[1], "mode ") == params[1] )
-        irc_cmd_channel_mode (session, params[0], params[1] + 5);
-
-    if ( strstr (params[1], "nick ") == params[1] )
-        irc_cmd_nick (session, params[1] + 5);
-
-    if ( strstr (params[1], "whois ") == params[1] )
-        irc_cmd_whois (session, params[1] + 5);
-}
-
-
-void irc_event_dcc_chat (irc_session_t * session, const char * nick, const char * addr, irc_dcc_t dccid)
-{
-    printf ("DCC chat [%d] requested from '%s' (%s)\n", dccid, nick, addr);
-
-    irc_dcc_accept (session, dccid, 0, dcc_recv_callback);
-}
-
-
 void irc_event_dcc_send (irc_session_t * session, const char * nick, const char * addr, const char * filename, unsigned long size, irc_dcc_t dccid)
 {
     FILE * fp;
@@ -277,15 +135,6 @@ void irc_event_dcc_send (irc_session_t * session, const char * nick, const char 
 
     irc_dcc_accept (session, dccid, fp, dcc_file_recv_callback);
 }
-
-void event_numeric (irc_session_t * session, unsigned int event, const char * origin, const char ** params, unsigned int count)
-{
-    char buf[24];
-    sprintf (buf, "%d", event);
-
-    dump_event (session, buf, origin, params, count);
-}
-
 
 int main (int argc, char **argv)
 {
@@ -304,23 +153,7 @@ int main (int argc, char **argv)
 
     callbacks.event_connect = event_connect;
     callbacks.event_join = event_join;
-    callbacks.event_nick = dump_event;
-    callbacks.event_quit = dump_event;
-    callbacks.event_part = dump_event;
-    callbacks.event_mode = dump_event;
-    callbacks.event_topic = dump_event;
-    callbacks.event_kick = dump_event;
-    callbacks.event_channel = event_channel;
     callbacks.event_privmsg = event_privmsg;
-    callbacks.event_notice = dump_event;
-    callbacks.event_invite = dump_event;
-    callbacks.event_umode = dump_event;
-    callbacks.event_ctcp_rep = dump_event;
-    callbacks.event_ctcp_action = dump_event;
-    callbacks.event_unknown = dump_event;
-    callbacks.event_numeric = event_numeric;
-
-    callbacks.event_dcc_chat_req = irc_event_dcc_chat;
     callbacks.event_dcc_send_req = irc_event_dcc_send;
 
     s = irc_create_session (&callbacks);
