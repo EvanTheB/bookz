3bd9156fcd9fa46b35a26508c973bd9fb7e31cc3
EvanTheB <evanbenn@gmail.com>
2017-09-12 20:51:40 +0800

    add extract thread, three thread synchro.

--- bookz.c
+++ bookz.c
@@ -32,9 +32,13 @@
 #include <pthread.h>
 #include <errno.h>
 #include <unistd.h>
+#include <sys/select.h>
+#include <sys/eventfd.h>
+
 
 #include "libircclient.h"
 #include "archive.h"
+#include "archive_entry.h"
 
 
 /*
@@ -44,30 +48,8 @@ typedef struct
 {
     char    * channel;
     char    * nick;
-
+    struct file_thread_args* file_thread;
 } irc_ctx_t;
-
-
-struct thread_args
-{
-    irc_session_t *session;
-    char *channel;
-};
-static void * thread_function(void * vargs)
-{
-    struct thread_args *args = vargs;
-    while ( 1 )
-    {
-        char buffer[200];
-        char *buf = buffer;
-        size_t buffer_size = 200;
-        getline(&buf, &buffer_size,stdin);
-        irc_cmd_msg (args->session, args->channel, buf);
-    }
-    free(args->channel);
-    free(vargs);
-    return 0;
-}
 
 static int
 copy_data(struct archive *ar, struct archive *aw)
@@ -91,59 +73,165 @@ copy_data(struct archive *ar, struct archive *aw)
   }
 }
 
-static void
+static char*
 extract(const char *filename)
 {
-  struct archive *a;
-  struct archive *ext;
-  struct archive_entry *entry;
-  int flags;
-  int r;
+    struct archive *a;
+    struct archive *ext;
+    struct archive_entry *entry;
+    int flags;
+    int r;
 
-  /* Select which attributes we want to restore. */
-  flags = ARCHIVE_EXTRACT_TIME;
-  flags |= ARCHIVE_EXTRACT_PERM;
-  flags |= ARCHIVE_EXTRACT_ACL;
-  flags |= ARCHIVE_EXTRACT_FFLAGS;
+    char *search_result_fname = NULL;
 
-  a = archive_read_new();
-  archive_read_support_format_all(a);
-  archive_read_support_compression_all(a);
-  ext = archive_write_disk_new();
-  archive_write_disk_set_options(ext, flags);
-  archive_write_disk_set_standard_lookup(ext);
-  if ((r = archive_read_open_filename(a, filename, 10240)))
-    exit(1);
-  for (;;) {
-    r = archive_read_next_header(a, &entry);
-    if (r == ARCHIVE_EOF)
-      break;
-    if (r < ARCHIVE_OK)
-      fprintf(stderr, "%s\n", archive_error_string(a));
-    if (r < ARCHIVE_WARN)
-      exit(1);
-    r = archive_write_header(ext, entry);
-    if (r < ARCHIVE_OK)
-      fprintf(stderr, "%s\n", archive_error_string(ext));
-    else if (archive_entry_size(entry) > 0) {
-      r = copy_data(a, ext);
-      if (r < ARCHIVE_OK)
-        fprintf(stderr, "%s\n", archive_error_string(ext));
-      if (r < ARCHIVE_WARN)
+    /* Select which attributes we want to restore. */
+    flags = ARCHIVE_EXTRACT_TIME;
+    flags |= ARCHIVE_EXTRACT_PERM;
+    flags |= ARCHIVE_EXTRACT_ACL;
+    flags |= ARCHIVE_EXTRACT_FFLAGS;
+
+    a = archive_read_new();
+    archive_read_support_format_all(a);
+    archive_read_support_filter_all(a);
+    ext = archive_write_disk_new();
+    archive_write_disk_set_options(ext, flags);
+    archive_write_disk_set_standard_lookup(ext);
+    if ((r = archive_read_open_filename(a, filename, 10240)))
         exit(1);
+    for (;;) {
+            r = archive_read_next_header(a, &entry);
+        if (r == ARCHIVE_EOF)
+          break;
+
+        if (r < ARCHIVE_OK)
+          fprintf(stderr, "%s\n", archive_error_string(a));
+        if (r < ARCHIVE_WARN)
+          exit(1);
+
+        const char *fname = archive_entry_pathname(entry);
+        if (strncasecmp(fname, "search", 6) == 0)
+        {
+            search_result_fname = realloc(search_result_fname, strlen(fname) + 1);
+            strcpy(search_result_fname, fname);
+        }
+
+        r = archive_write_header(ext, entry);
+        if (r < ARCHIVE_OK)
+          fprintf(stderr, "%s\n", archive_error_string(ext));
+        else if (archive_entry_size(entry) > 0) {
+          r = copy_data(a, ext);
+          if (r < ARCHIVE_OK)
+            fprintf(stderr, "%s\n", archive_error_string(ext));
+          if (r < ARCHIVE_WARN)
+            exit(1);
+        }
+
+        r = archive_write_finish_entry(ext);
+        if (r < ARCHIVE_OK)
+          fprintf(stderr, "%s\n", archive_error_string(ext));
+        if (r < ARCHIVE_WARN)
+          exit(1);
     }
-    r = archive_write_finish_entry(ext);
-    if (r < ARCHIVE_OK)
-      fprintf(stderr, "%s\n", archive_error_string(ext));
-    if (r < ARCHIVE_WARN)
-      exit(1);
-  }
-  archive_read_close(a);
-  archive_read_free(a);
-  archive_write_close(ext);
-  archive_write_free(ext);
-  exit(0);
+    archive_read_close(a);
+    archive_read_free(a);
+    archive_write_close(ext);
+    archive_write_free(ext);
+
+    return search_result_fname;
 }
+
+static void
+search_result_display(const char* fname)
+{
+    FILE* fp = fopen(fname, "r");
+    char *buf = NULL;
+    size_t buffer_size = 0;
+    int counter = 0;
+    while (-1 != getline(&buf, &buffer_size, fp))
+    {
+        printf("%d: %s", counter++, buf);
+    }
+    fclose(fp);
+    free(buf);
+}
+
+struct control_thread_args
+{
+    irc_session_t *session;
+    char *channel;
+    int extract_efd;
+    char *extract_fname;
+};
+static void * control_thread(void * vargs)
+{
+    struct control_thread_args *args = vargs;
+    fd_set rfds;
+    int retval;
+    char *buf = NULL;
+    size_t buffer_size = 0;
+
+    while ( 1 )
+    {
+        /* Watch stdin (fd 0) to see when it has input. */
+        FD_ZERO(&rfds);
+        FD_SET(0, &rfds);
+        FD_SET(args->extract_efd, &rfds);
+
+        retval = select(args->extract_efd + 1, &rfds, NULL, NULL, NULL);
+        if (retval < 0)
+        {
+            printf("select error\n");
+        }
+
+        if (FD_ISSET(0, &rfds))
+        {
+            getline(&buf, &buffer_size, stdin);
+            irc_cmd_msg (args->session, args->channel, buf);
+        }
+        if (FD_ISSET(args->extract_efd, &rfds))
+        {
+            uint64_t tmp;
+            read(args->extract_efd, &tmp, sizeof(uint64_t));
+            search_result_display(args->extract_fname);
+        }
+    }
+
+    free(buf);
+    free(vargs);
+    return 0;
+}
+
+struct file_thread_args
+{
+    struct control_thread_args* control_thread;
+    pthread_mutex_t mtx;
+    char *fname;
+};
+static void * file_thread(void * vargs)
+{
+    struct file_thread_args *args = vargs;
+    while(1)
+    {
+        // block
+        pthread_mutex_lock(&args->mtx);
+        // extract
+        printf("Extracting %s\n", args->fname);
+        char *search = extract(args->fname);
+        // notify
+        if (search)
+        {
+            printf("Search result found %s\n", search);
+            free(args->control_thread->extract_fname);
+            args->control_thread->extract_fname = search;
+            uint64_t tmp = 1;
+            write(args->control_thread->extract_efd, &tmp, sizeof(uint64_t));
+        }
+    }
+    free(vargs);
+    return 0;
+}
+
+
 
 
 
@@ -153,13 +241,24 @@ void event_join (irc_session_t * session, const char * event, const char * origi
     irc_cmd_user_mode (session, "+i");
 
     pthread_t tid;
-    struct thread_args* ta = malloc(sizeof(ta));
-    ta->session = session;
-    ta->channel = malloc(strlen(params[0]) + 1);
-    strcpy(ta->channel, params[0]);
+    struct control_thread_args* ct = malloc(sizeof(struct control_thread_args));
+    ct->session = session;
+    ct->channel = ((irc_ctx_t *)irc_get_ctx(session))->channel;
+    ct->extract_efd = eventfd(0, 0);
+    ct->extract_fname = NULL;
 
-    if (pthread_create (&tid, 0, thread_function, ta) != 0)
+    if (pthread_create (&tid, NULL, control_thread, ct) != 0)
         printf ("CREATE_THREAD failed: %s\n", strerror(errno));
+
+    struct file_thread_args* ft = malloc(sizeof(struct file_thread_args));
+    ft->control_thread = ct;
+    pthread_mutex_init(&ft->mtx, NULL);
+    pthread_mutex_lock(&ft->mtx);
+    ft->fname = NULL;
+    if (pthread_create (&tid, NULL, file_thread, ft) != 0)
+        printf ("CREATE_THREAD failed: %s\n", strerror(errno));
+
+    ((irc_ctx_t *)irc_get_ctx(session))->file_thread = ft;
 }
 
 
@@ -180,21 +279,27 @@ void event_privmsg (irc_session_t * session, const char * event, const char * or
         params[0], params[1] );
 }
 
-struct dcc_data
+struct dcc_ctx
 {
     FILE* fp;
     char* fname;
 };
 void dcc_file_recv_callback (irc_session_t * session, irc_dcc_t id, int status, void * ctx, const char * data, unsigned int length)
 {
-    struct dcc_data* fdata = ctx;
+    struct dcc_ctx* fdata = ctx;
+    irc_ctx_t* session_ctx = irc_get_ctx(session);
+
     if ( status == 0 && length == 0 )
     {
         printf ("File sent successfully\n");
 
         if ( fdata->fp )
             fclose (fdata->fp);
-        extract(fdata->fname);
+
+        free(session_ctx->file_thread->fname);
+        session_ctx->file_thread->fname = fdata->fname;
+        pthread_mutex_unlock(&session_ctx->file_thread->mtx);
+
         free(fdata);
     }
     else if ( status )
@@ -203,6 +308,7 @@ void dcc_file_recv_callback (irc_session_t * session, irc_dcc_t id, int status, 
 
         if ( fdata->fp )
             fclose (fdata->fp);
+        free(fdata);
     }
     else
     {
@@ -214,14 +320,13 @@ void dcc_file_recv_callback (irc_session_t * session, irc_dcc_t id, int status, 
 
 void irc_event_dcc_send (irc_session_t * session, const char * nick, const char * addr, const char * filename, unsigned long size, irc_dcc_t dccid)
 {
-    FILE * fp;
+    struct dcc_ctx* ctx = malloc(sizeof(struct dcc_ctx));
+
     printf ("DCC send [%d] requested from '%s' (%s): %s (%lu bytes)\n", dccid, nick, addr, filename, size);
 
-    if ( (fp = fopen (filename, "wb")) == 0 )
+    if ( (ctx->fp = fopen (filename, "wb")) == 0 )
         abort();
 
-    struct dcc_data* ctx = malloc(sizeof(ctx));
-    ctx->fp = fp;
     ctx->fname = malloc(strlen(filename) + 1);
     strcpy(ctx->fname, filename);
     irc_dcc_accept (session, dccid, ctx, dcc_file_recv_callback);
